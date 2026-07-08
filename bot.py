9b88027603e67d957bedfcd27b39e18e131f1af9
Amyrmohseni <72305958+Amyrmohseni@users.noreply.github.com>
2026-07-08 20:52:30 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,59 @@
+import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = "8800531130:AAEil87ibT4pmUpOTg7Er8ebRty_vpwOyV4"
+TOKEN = "YOUR_NEW_TOKEN_HERE"
 CHANNEL = "@vibesof23"
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.args:
-        msg = int(context.args[0])
-        await context.bot.copy_message(
-            chat_id=update.effective_chat.id,
-            from_chat_id=CHANNEL,
-            message_id=msg
-        )
+        try:
+            msg_id = int(context.args[0])
+
+            await context.bot.copy_message(
+                chat_id=update.effective_chat.id,
+                from_chat_id=CHANNEL,
+                message_id=msg_id
+            )
+
+        except Exception as e:
+            await update.message.reply_text(
+                f"خطا در ارسال پیام:\n{e}"
+            )
+
     else:
-        await update.message.reply_text("لینک نامعتبر است.")
+        await update.message.reply_text(
+            "لینک نامعتبر است."
+        )
+
 
 async def get_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.forward_origin:
+    if update.message and update.message.forward_origin:
         await update.message.reply_text(
             f"Message ID: {update.message.forward_origin.message_id}"
         )
     else:
-        await update.message.reply_text("پیام را از کانال فوروارد کن.")
+        await update.message.reply_text(
+            "یک پیام را از کانال فوروارد کن."
+        )
 
-app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.ALL, get_id))
+async def main():
+    app = Application.builder().token(TOKEN).build()
 
-print("Bot Started")
-app.run_polling()
+    app.add_handler(
+        CommandHandler("start", start)
+    )
+
+    app.add_handler(
+        MessageHandler(filters.ALL, get_id)
+    )
+
+    print("Bot Started")
+
+    await app.run_polling()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
